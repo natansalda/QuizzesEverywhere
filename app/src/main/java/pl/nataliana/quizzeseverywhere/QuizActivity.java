89860fe807799a9a767a0323d7b53fcbe465edf8
Natalia Nazaruk <natalia.nazaruk@assecods.pl>
2018-03-19 12:30:54 +0100

    Add: ayouts for quiz questions added

--- app/src/main/java/pl/nataliana/quizzeseverywhere/QuizActivity.java
+++ app/src/main/java/pl/nataliana/quizzeseverywhere/QuizActivity.java
@@ -1,12 +1,20 @@
 package pl.nataliana.quizzeseverywhere;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by natalia.nazaruk on 14.03.2018.
  */
 
-public class QuizActivity {
+public class QuizActivity extends AppCompatActivity{
 
     public static final String SINGLE_QUIZ_BASE_URL = "http://quiz.o2.pl/api/v1/quiz/%s/0";
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_detail);
+    }
 }
